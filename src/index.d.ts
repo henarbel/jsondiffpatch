acf0aa75ae1b6fc15228e15fb92d4812d91e3c33
Chen Arbel <CArbel@attentigroup.com>
2021-07-13 22:04:17 +0300

    removed html stuff from index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,22 +2,6 @@ export interface Formatter {
     format(delta: Delta, original: any): string;
 }
 
-export interface HtmlFormatter extends Formatter {
-    /**
-     * Set whether to show or hide unchanged parts of a diff.
-     * @param show Whether to show unchanged parts
-     * @param node The root element the diff is contained within. (Default: body)
-     * @param delay Transition time in ms. (Default: no transition)
-     */
-    showUnchanged(show: boolean, node?: Element | null, delay?: number): void;
-
-    /**
-     * An alias for showUnchanged(false, ...)
-     * @param node The root element the diff is contained within (Default: body)
-     * @param delay Transition time in ms. (Default: no transition)
-     */
-    hideUnchanged(node?: Element | null, delay?: number): void;
-}
 
 export interface Delta {
     [key: string]: any;
@@ -189,7 +173,6 @@ export const create: (options?: any) => DiffPatcher
 export const formatters: {
   annotated: Formatter;
   console: Formatter;
-  html: HtmlFormatter;
 };
 
 export const console: Formatter
